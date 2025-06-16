08dde48082f7cd81155988592ee634cb9b9cdaa8
lislenytorquato <lislenytorquato19@gmail.com>
2025-06-16 17:29:44 -0300

    PedidoTest pt 1

--- src/main/java/questao20/supermercado/service/PedidoService.java
+++ src/main/java/questao20/supermercado/service/PedidoService.java
@@ -1,9 +1,35 @@
 package questao20.supermercado.service;
 
 import org.springframework.stereotype.Service;
+import questao20.supermercado.model.Item;
+import questao20.supermercado.model.Pedido;
+import questao20.supermercado.model.Produto;
+
+import java.util.List;
 
 @Service
 public class PedidoService {
+
+    private final Pedido pedido;
+
+    public PedidoService(Pedido pedido){
+        this.pedido = pedido;
+    }
+
+    public void criarPedido(List<Item> listaDeItens, Double valorTotalDoPedido){
+        pedido.setListaDeItens(listaDeItens);
+        pedido.setValorTotalDoPedido(valorTotalDoPedido);
+    }
+
+    public Double retornarTroco(Double valorRecebido){
+        return pedido.retornarTroco(valorRecebido);
+    }
+    public void calculaValorTotal(){
+        pedido.calculaValorTotal();
+    }
+    public Boolean adicionaItemNaLista(Produto produto, Integer quantidade){
+        return pedido.adicionaItemNaLista(produto,quantidade);
+    }
 
 
 
